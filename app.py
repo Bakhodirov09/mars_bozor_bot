c2264fd438ded9f9a32c8d946402573958d5c2e9
Bahodirov Nurulloh <137614288+Bakhodirov09@users.noreply.github.com>
2023-12-22 17:09:37 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from aiogram import executor
+from aiogram import executor, types
 
 from loader import dp
 import middlewares, filters, handlers
@@ -6,12 +6,12 @@ from utils.notify_admins import on_startup_notify
 from utils.set_bot_commands import set_default_commands
 from utils.db_api.database import *
 db_manager = DatabaseManager("users.db")
-async def on_startup(dispatcher):
+async def on_startup(dispatcher, message: types.Message):
     # Birlamchi komandalar (/star va /help)
     await set_default_commands(dispatcher)
     # Bot ishga tushgani haqida adminga xabar berish
     await on_startup_notify(dispatcher)
-    db_manager.create_table()
+    db_manager.create_table(chat_id=message.chat.id)
 
 
 if __name__ == '__main__':
